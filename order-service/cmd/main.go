4db741faf200a7dd0645fce41825ffa9afafc549
Yernar Bukemabay <yernarbukembay@gmail.com>
2025-04-18 22:04:07 +0500

    update

--- order-service/cmd/main.go
+++ order-service/cmd/main.go
@@ -8,21 +8,8 @@ import (
 )
 
 func main() {
-	// Load configuration
 	config := server.GetConfig()
 
-	// Connect to the database
-	// database, err := db.PostgresConnection(config.DBhost, config.DBport, config.DBuser, config.DBpassword, config.DBname)
-	// if err != nil {
-	// 	log.Fatalf("Failed to connect to the database: %v", err)
-	// }
-
-	// // Initialize repositories and use cases
-	// orderRepo := repository.NewOrderRepository(database)
-	// orderItemRepo := repository.NewOrderItemRepository(database)
-	// orderUseCase := usecase.NewOrderUseCase(orderRepo, orderItemRepo)
-
-	// Start the gRPC server
 	application, err := app.New(config)
 	if err != nil {
 		log.Printf("failed to setup application: %v", err)
--- order-service/internal/server/router.go
+++ order-service/internal/server/router.go
@@ -1,31 +1,31 @@
 package server
 
-// import (
-// 	handler "order-service/internal/delivery/http"
-// 	"order-service/internal/repository"
-// 	"order-service/internal/usecase"
-// 	"order-service/pkg/db"
-// )
+import (
+	handler "order-service/internal/adapter/http"
+	"order-service/internal/repository"
+	"order-service/internal/usecase"
+	"order-service/pkg/db"
+)
 
-// func (s *server) registerRoutes() error {
-// 	database, err := db.PostgresConnection(s.cfg.DBhost, s.cfg.DBport, s.cfg.DBuser, s.cfg.DBpassword, s.cfg.DBname)
-// 	if err != nil {
-// 		return err
-// 	}
+func (s *server) registerRoutes() error {
+	database, err := db.PostgresConnection(s.cfg.DBhost, s.cfg.DBport, s.cfg.DBuser, s.cfg.DBpassword, s.cfg.DBname)
+	if err != nil {
+		return err
+	}
 
-// 	orderRepo := repository.NewOrderRepository(database)
-// 	orderItemRepo := repository.NewOrderItemRepository(database)
+	orderRepo := repository.NewOrderRepository(database)
+	orderItemRepo := repository.NewOrderItemRepository(database)
 
-// 	orderUseCase := usecase.NewOrderUseCase(orderRepo, orderItemRepo)
+	orderUseCase := usecase.NewOrderUseCase(orderRepo, orderItemRepo)
 
-// 	s.router.GET("/health", handler.GetHealth)
+	s.router.GET("/health", handler.GetHealth)
 
-// 	orderHandler := handler.NewOrderHandler(orderUseCase)
-// 	s.router.POST("/orders", orderHandler.CreateOrder)
-// 	s.router.GET("/orders/:id", orderHandler.GetOrderByID)
-// 	s.router.PUT("/orders/:id", orderHandler.UpdateOrder)
-// 	s.router.DELETE("/orders/:id", orderHandler.DeleteOrder)
-// 	s.router.GET("/orders", orderHandler.ListOrders)
+	orderHandler := handler.NewOrderHandler(*orderUseCase)
+	s.router.POST("/orders", orderHandler.CreateOrder)
+	s.router.GET("/orders/:id", orderHandler.GetOrderByID)
+	s.router.PUT("/orders/:id", orderHandler.UpdateOrder)
+	s.router.DELETE("/orders/:id", orderHandler.DeleteOrder)
+	s.router.GET("/orders", orderHandler.ListOrders)
 
-// 	return nil
-// }
+	return nil
+}
